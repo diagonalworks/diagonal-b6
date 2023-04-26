edb47c823126a9b1a9b58c2206397a8b487995f3
Andrew Eland <andrew@andreweland.org>
2023-04-26 22:16:26 +0100

    Optionally copy all fields from a shapefile.

--- src/diagonal.works/b6/ingest/gdal/source_test.go
+++ src/diagonal.works/b6/ingest/gdal/source_test.go
@@ -34,13 +34,46 @@ func TestReadFeaturesFromLSOABoundaries(t *testing.T) {
 	}
 
 	if found != nil {
-		boundary := b6.Tag{Key: "#boundary", Value: "lsoa"}
-		if found.Get("#boundary") != boundary {
-			t.Errorf("Expected to find %s", boundary)
+		expected := []b6.Tag{{Key: "#boundary", Value: "lsoa"}, {Key: "name", Value: "Camden 018B"}}
+		for _, tag := range expected {
+			if found.Get(tag.Key) != tag {
+				t.Errorf("Expected to find %s", tag)
+			}
 		}
-		name := b6.Tag{Key: "name", Value: "Camden 018B"}
-		if found.Get("name") != name {
-			t.Errorf("Expected to find %s", name)
+	} else {
+		t.Errorf("Expected to find boundary")
+	}
+}
+
+func TestReadFeaturesFromLSOABoundariesCopyingAllFields(t *testing.T) {
+	source := Source{
+		Filename:      test.Data("lsoa-camden.shp"),
+		CopyAllFields: true,
+		CopyTags:      []CopyTag{{Field: "LSOA11CD", Key: "code"}},
+		AddTags:       []b6.Tag{{Key: "#boundary", Value: "lsoa"}},
+		IDField:       "LSOA11CD",
+		IDStrategy:    GBONS2011IDStrategy,
+		Bounds:        s2.FullRect(),
+	}
+
+	var found ingest.Feature
+	emit := func(f ingest.Feature, goroutine int) error {
+		if f.FeatureID() == b6.FeatureIDFromGBONSCode("E01000858", 2011, b6.FeatureTypeArea) {
+			found = f
+		}
+		return nil
+	}
+	err := source.Read(ingest.ReadOptions{}, emit, context.Background())
+	if err != nil {
+		t.Errorf("Expected no error, found %s", err)
+		return
+	}
+	if found != nil {
+		expected := []b6.Tag{{Key: "code", Value: "E01000858"}, {Key: "LSOA11NM", Value: "Camden 018B"}}
+		for _, tag := range expected {
+			if found.Get(tag.Key) != tag {
+				t.Errorf("Expected to find %s", tag)
+			}
 		}
 	} else {
 		t.Errorf("Expected to find boundary")
