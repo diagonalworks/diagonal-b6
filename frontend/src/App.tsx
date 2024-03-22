7317b079519f3b6b4210ad268ab142029251dd4d
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2024-03-22 09:52:30 +0000

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { Map } from './components/Map';
+
 function App() {
     return (
-        <>
-            <h1 className="text-xl text-violet-80 ">Vite + React</h1>
-        </>
+        <div className="container mx-auto pt-10 ">
+            <h1 className="text-xl text-center text-violet-80 ">
+                Vite + React
+            </h1>
+            <Map />
+        </div>
     );
 }
 
