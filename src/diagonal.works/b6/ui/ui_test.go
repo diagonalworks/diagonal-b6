e301d00139e1261e9f2342b1712138c1d07b81ec
Andrew Eland <andrew@andreweland.org>
2024-05-11 07:33:10 +0100

    Use Evaluator for /stack and new /evaluate endpoints. (#221)

--- src/diagonal.works/b6/ui/ui_test.go
+++ src/diagonal.works/b6/ui/ui_test.go
@@ -4,15 +4,18 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"sync"
 	"testing"
 
 	"diagonal.works/b6"
+	"diagonal.works/b6/api"
 	"diagonal.works/b6/api/functions"
 	"diagonal.works/b6/ingest"
 	"diagonal.works/b6/test/camden"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestStateFilledFromStartupQuery(t *testing.T) {
@@ -69,9 +72,13 @@ func TestEvaluateFunctionThatChangesWorld(t *testing.T) {
 	var lock sync.RWMutex
 	handler := StackHandler{
 		UI: &OpenSourceUI{
-			Worlds:          worlds,
-			FunctionSymbols: functions.Functions(),
-			Lock:            &lock,
+			Worlds: worlds,
+			Lock:   &lock,
+			Evaluator: api.Evaluator{
+				Worlds:          worlds,
+				FunctionSymbols: functions.Functions(),
+				Lock:            &lock,
+			},
 		},
 	}
 
@@ -103,5 +110,72 @@ func TestEvaluateFunctionThatChangesWorld(t *testing.T) {
 	expected := b6.Tag{Key: "building:levels", Value: b6.String("25")}
 	if levels := f.Get(expected.Key); levels != expected {
 		t.Errorf("expected tag %s, found %s", expected, levels)
+	}
+}
+func TestEvaluateFunctionViaEvaluateEndpoint(t *testing.T) {
+	worlds := &ingest.MutableWorlds{
+		Base: camden.BuildGranarySquareForTests(t),
+	}
+	var lock sync.RWMutex
+	handler := EvaluateHandler{
+		Evaluator: api.Evaluator{
+			Worlds:          worlds,
+			FunctionSymbols: functions.Functions(),
+			Adaptors:        functions.Adaptors(),
+			Lock:            &lock,
+		},
+	}
+
+	url := "http://b6.diagonal.works/evaluate"
+	j := map[string]interface{}{
+		"request": map[string]interface{}{
+			"call": map[string]interface{}{
+				"function": map[string]interface{}{
+					"symbol": "add-ints",
+				},
+				"args": []map[string]interface{}{
+					{
+						"literal": map[string]interface{}{
+							"intValue": 22,
+						},
+					},
+					{
+						"literal": map[string]interface{}{
+							"intValue": 20,
+						},
+					},
+				},
+			},
+		},
+		"root": map[string]interface{}{
+			"type":      b6.FeatureTypeCollection,
+			"namespace": "diagonal.works/test/world",
+			"value":     0,
+		},
+	}
+	body, _ := json.Marshal(j)
+	request := httptest.NewRequest("POST", url, bytes.NewBuffer(body))
+	response := httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	result := response.Result()
+	if result.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status %d, found %d", http.StatusOK, result.StatusCode)
+	}
+	body, err := io.ReadAll(response.Result().Body)
+	if err != nil {
+		t.Fatalf("Expected no error, found %s", err)
+	}
+
+	expected := map[string]interface{}{
+		"result": map[string]interface{}{
+			"literal": map[string]interface{}{
+				"intValue": "42",
+			},
+		},
+	}
+	var actual map[string]interface{}
+	json.Unmarshal(body, &actual)
+	if diff := cmp.Diff(expected, actual); diff != "" {
+		t.Errorf("Expected no difference, found: %s", diff)
 	}
 }
