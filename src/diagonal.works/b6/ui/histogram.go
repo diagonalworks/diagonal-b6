ddbda88aa027ffa024ce7361e6f38d96a0887fc0
Andrew Eland <andrew@andreweland.org>
2024-05-15 13:13:37 +0100

    Show destinaton icons for origin/destination histograms.

--- src/diagonal.works/b6/ui/histogram.go
+++ src/diagonal.works/b6/ui/histogram.go
@@ -3,7 +3,9 @@ package ui
 import (
 	"diagonal.works/b6"
 	"diagonal.works/b6/api"
+	"diagonal.works/b6/geojson"
 	pb "diagonal.works/b6/proto"
+	"diagonal.works/b6/renderer"
 )
 
 func addLabel(response *UIResponseJSON, f b6.Feature) {
@@ -76,5 +78,43 @@ func fillResponseFromHistogramFeature(response *UIResponseJSON, c b6.CollectionF
 		Q:      c.FeatureID().String(),
 		Before: pb.MapLayerPosition_MapLayerPositionEnd,
 	})
+
+	if b6.CanAdaptCollection[any, b6.FeatureID](c) {
+		ids := b6.AdaptCollection[any, b6.FeatureID](c)
+		err = fillResponseFromHistogramFeatures(response, ids, w)
+	}
+
+	return err
+}
+
+func fillResponseFromHistogramFeatures(response *UIResponseJSON, c b6.Collection[any, b6.FeatureID], w b6.World) error {
+	i := c.Begin()
+	values := make(map[b6.FeatureID]struct{})
+	for {
+		ok, err := i.Next()
+		if err != nil {
+			return err
+		} else if !ok {
+			break
+		}
+		values[i.Value()] = struct{}{}
+	}
+
+	g := geojson.NewFeatureCollection()
+	for v := range values {
+		if f := w.FindFeatureByID(v); f != nil {
+			if p, ok := f.(b6.PhysicalFeature); ok {
+				if centroid, ok := b6.Centroid(p); ok {
+					gf := geojson.NewFeatureFromS2Point(centroid)
+					icon, _ := renderer.IconForFeature(f)
+					gf.Properties["-b6-icon"] = icon
+					g.AddFeature(gf)
+				}
+			}
+		}
+	}
+	if len(g.Features) > 0 {
+		response.AddGeoJSON(g)
+	}
 	return nil
 }
