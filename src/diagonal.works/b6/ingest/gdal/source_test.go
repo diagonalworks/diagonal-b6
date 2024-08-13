0cd0bf934c13a61610e375a8a04e84179fb94203
Andrew Eland <andrew@andreweland.org>
2024-08-13 10:56:06 +0100

    Correctly ingest shapefile polygons with holes. (#291)

--- src/diagonal.works/b6/ingest/gdal/source_test.go
+++ src/diagonal.works/b6/ingest/gdal/source_test.go
@@ -2,6 +2,7 @@ package gdal
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"diagonal.works/b6"
@@ -73,5 +74,47 @@ func TestReadFeaturesFromLSOABoundariesCopyingAllFields(t *testing.T) {
 		if found.Get(tag.Key) != tag {
 			t.Errorf("Expected to find %s", tag)
 		}
+	}
+}
+
+func TestReadFeatureFromWardBoundaryWithHole(t *testing.T) {
+	source := Source{
+		Filename:   test.Data("ward-hole.shp"),
+		AddTags:    []b6.Tag{{Key: "#boundary", Value: b6.NewStringExpression("ward")}},
+		IDField:    "WD21CD",
+		IDStrategy: UKONS2021IDStrategy,
+		Bounds:     s2.FullRect(),
+	}
+	var found *ingest.AreaFeature
+	emit := func(f ingest.Feature, goroutine int) error {
+		if f.FeatureID() == b6.FeatureIDFromUKONSCode("E05003517", 2021, b6.FeatureTypeArea) {
+			if a, ok := f.(*ingest.AreaFeature); ok {
+				found = a
+			} else {
+				return fmt.Errorf("expected an area feature, found %T", f)
+			}
+		}
+		return nil
+	}
+	err := source.Read(ingest.ReadOptions{}, emit, context.Background())
+	if err != nil {
+		t.Fatalf("expected no error, found %s", err)
+	}
+	if found == nil {
+		t.Fatal("expected to find boundary")
+	}
+
+	if l := found.Len(); l != 1 {
+		t.Fatalf("expected one polygon, found %d", l)
+	}
+
+	polygon, ok := found.Polygon(0)
+	if !ok {
+		t.Fatalf("expected a polygon from points, not path IDs")
+	}
+
+	inside := s2.LatLngFromDegrees(50.85720, -3.40841)
+	if !polygon.ContainsPoint(s2.PointFromLatLng(inside)) {
+		t.Fatalf("expected polygon to contain point")
 	}
 }
