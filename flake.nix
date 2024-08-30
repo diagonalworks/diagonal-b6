309fb4633fa3de5f5da9ffb0a3614269c2981bc3
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2024-08-30 14:39:52 +0100

    Build python project properly; including generated files

--- flake.nix
+++ flake.nix
@@ -110,15 +110,45 @@
         ${b6-go}/bin/b6-api --docs --functions | ${pkgs.lib.getExe pkgs.jq} ".Functions[] | select(.Name == \"''$1\")"
       '';
 
-      b6-py = python.pkgs.buildPythonPackage
-        (pythonProject.renderers.buildPythonPackage
-          {
-            inherit python;
-          } // {
-          patchPhase = ''
-            cat ${pyproject-file} > pyproject.toml
-          '';
-        });
+      renderedPyProject = pythonProject.renderers.buildPythonPackage {
+        inherit python;
+      };
+
+      b6-py = python.pkgs.buildPythonPackage (renderedPyProject // {
+        # Set the pyproject to be the one we computed via our b6 binary.
+        patchPhase = ''
+          cat ${pyproject-file} > pyproject.toml
+        '';
+
+        nativeBuildInputs = renderedPyProject.nativeBuildInputs ++ [
+          python.pkgs.grpcio-tools
+        ];
+
+        # A couple of hacks necessary to build the proto files and the API.
+        preBuild = ''
+          # Bring in the necessary proto files and the Makefile
+          cp -r ${./proto} ./proto
+          cat ${./Makefile} > some-Makefile
+
+          # Hack: Run the b6-api command outside of the Makefile, using our
+          # version.
+          ${b6-go}/bin/b6-api --functions | python diagonal_b6/generate_api.py > diagonal_b6/api_generated.py
+
+          # Hack: Make the directory structure that the Makefile expects,
+          # then move things to where we want them
+          mkdir python
+          mkdir python/diagonal_b6
+
+          make proto-python -f some-Makefile
+
+          # Cleanup
+          mv python/diagonal_b6/* ./diagonal_b6
+          rm -rf python/diagonal_b6
+        '';
+
+        pythonImportsCheck = [ "diagonal_b6" ];
+      });
+
 
       pythonEnv = python.withPackages (ps:
         [
