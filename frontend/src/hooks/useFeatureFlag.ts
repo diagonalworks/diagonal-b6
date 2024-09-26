7daeca07472a1e92b76d0a69af17b4d707dc67a0
gala <gala@diagonal.works>
2024-09-26 22:39:31 +0100

    require explicit feature flags (#312)
    * require explicit feature flags
    
    * isUndefined removed as not used

--- frontend/src/hooks/useFeatureFlag.ts
+++ frontend/src/hooks/useFeatureFlag.ts
@@ -1,4 +1,3 @@
-import { isUndefined } from 'lodash';
 import { useEffect, useState } from 'react';
 
 import { $IntentionalAny } from '@/utils/defs';
@@ -10,8 +9,8 @@ const useFeatureFlag = (flagName: string): boolean => {
         const envVariableName = `VITE_FEATURES_${flagName.toUpperCase()}`;
         const flag = import.meta.env[envVariableName as $IntentionalAny];
 
-        // If the flag is not explicitly set to false, we consider it to be true
-        if (flag === 'true' || isUndefined(flag)) {
+        // If the flag is not explicitly set to true, we consider it to be false
+        if (flag === 'true') {
             setFlagValue(true);
         }
     }, [flagName]);
